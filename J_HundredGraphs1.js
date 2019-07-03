86ee33011eb31a32cda3e318d2805456f533c87b
stan podolski <stanp@xlazz.com>
2019-07-02 23:31:30 -0500

    upd: working js and lua, vars are saved

--- J_HundredGraphs1.js
+++ J_HundredGraphs1.js
@@ -1,20 +1,163 @@
 var HundredGraphs = (function (api) {
-	var serviceId = 'urn:hundredgraphs-com:serviceId:HundredGraphs1';
-    var uuid = '4d494342-5342-5645-01e6-000002fb37e3';
     var myModule = {};
+    
+    var HG_SID = 'urn:hundredgraphs-com:serviceId:HundredGraphs1';
+    var uuid = '4d494342-5342-5645-01e6-000002fb37e3';
+    var device = api.getCpanelDeviceId();
+    
+    var hcg_deviceData = [];
     
     function about() {
         try {              
             var html = '<div>This is all about me !</div>';
+            html = html + '<div>HG_SID = ' + HG_SID + '</div>';
             api.setCpanelContent(html);
         } catch (e) {
             Utils.logError('Error in MyPlugin.about(): ' + e);
         }
     }
 
+    function updateEnabled(idx, object) {
+        hcg_deviceData[idx].enabled = (object.checked === true) ? "checked" : "";
+        console.log('HG enabled sw: ', hcg_deviceData[idx]);
+    }
+
+    function getListDevices(){
+        var deviceData = api.getUserData().devices;
+        for (item of deviceData){
+            for (attr of item.states){
+                var checkIt = 'Watts'
+                if (attr.variable == checkIt){
+                    console.log('HundredGraphs found PM device:', item.id, item.name);
+                    var item = {
+                        deviceId: item.id,
+                        key: item.name,
+                        serviceId: 'urn:micasaverde-com:serviceId:EnergyMetering1',
+                        serviceVar: checkIt,
+                        enabled: false
+                    }
+                    hcg_deviceData.push(item)
+                }
+            }
+        }
+        console.log('HundredGraphs found PM devices:', hcg_deviceData.length);
+    }
+
+    function unpackDeviceData(device) {
+        var deviceData;
+        try {
+            hcg_deviceData = [];
+
+            deviceData = api.getDeviceState(device, HG_SID, "DeviceData");
+            if (deviceData === undefined || deviceData === "" || !deviceData) 
+                return console.log('HundredGraphs empty variable DeviceData:', deviceData);
+            deviceData = deviceData.split(';');
+            for (var i = 0; i < deviceData.length; i++) {
+                // Get the intervals.
+                var item = {};
+                var attr = deviceData[i].toString();
+                //_console('HG1: ' + i + ' ' + deviceData[i]);       
+                attr = deviceData[i].split(',');
+                //console.log('HG1b: ', typeof attr, attr);
+                for (var j = 0; j < attr.length; j++) {
+                    var key, val;
+                    
+                    key = attr[j].split('=')[0];
+                    if (!key) return;
+                    key = key.trim();
+                    val = attr[j].split('=')[1];
+                    if (!val) return;
+                    item[key] = val;
+                    //console.log('HG2: ', j, item, attr[j]);
+                }
+                hcg_deviceData.push(item);
+            }
+            console.log('HundredGraphs hcg_deviceData: ', hcg_deviceData);
+        } catch(e){
+            console.error('HundredGraphs err:', e, 'deviceData:', deviceData, 'hcg_deviceData:', hcg_deviceData);
+            Utils.logError('Error in HG.unpackDeviceData(): ' + e);
+        }
+    }
+
+    function packDeviceData(){
+        console.log('{HundredGraphs packDeviceData} hcg_deviceData: ', hcg_deviceData);
+        var deviceData = '';
+        for (item of hcg_deviceData){
+            console.log('{HundredGraphs packDeviceData} item: ', item);
+            deviceData = deviceData + 'deviceId=' + item.deviceId + ',key=' + item.key + ',serviceId=' + item.serviceId + ',serviceVar=' + item.serviceVar + ',enabled=' + item.enabled + ';\n ';
+        }
+        console.log('{HundredGraphs packDeviceData} deviceData: ', deviceData);
+        api.setDeviceStateVariable(device, HG_SID, "DeviceData", deviceData, 0);
+    }
+
+    
+    function showDevices() {
+        try {
+            var html = '';
+
+            unpackDeviceData(device);
+
+            if (hcg_deviceData.length > 0) {
+                // Area to display statuses and error messages.
+                html += '<p id="status_display" style="width:90%; position:relative; margin-left:auto; margin-right:auto; table-layout:fixed; text-align:center; color:black"></div>';
+
+                html += '<table style="width:90%; position:relative; margin-left:auto; margin-right:auto">';
+
+                // Show titles
+                html += '<tr>';
+                html += '<td style="font-weight:bold; text-align:center; width:30%">Device #</td>';
+                html += '<td style="font-weight:bold; text-align:center; width:30%">Device name</td>';
+                html += '<td style="font-weight:bold; text-align:center; width:10%"></td>';
+                html += '</tr>';
+
+                // Show device list
+                for (var i = 0; i < hcg_deviceData.length; i++) {
+                    html += '<tr>';
+                    html += '<td style="padding-left:5%">' + hcg_deviceData[i].deviceId + '</td>';
+                    html += '<td style="">' + api.getDisplayedDeviceName(hcg_deviceData[i].deviceId) + '</td>';
+                    html += '<td><input type="checkbox" value="' + hcg_deviceData[i].devNum + '" onClick="HundredGraphs.updateEnabled(' + i + ', this)" ' + hcg_deviceData[i].enabled + ' style="margin-left:42%" /></td>';
+                    html += '</td>';
+                    html += '</tr>';
+                }
+
+                // Create empty row
+                html += '<tr>';
+                html += '<td colspan="4"><br /></td>';
+                html += '</tr>';
+
+                // Display the 'Update' button
+                html += '<tr>';
+                html += '<td colspan="3"><input type="button" value="Update" onClick="HundredGraphs.packDeviceData()" style="margin-left:60%" /></td>';
+                html += '</tr>';
+
+                html += '</table>';
+            } else {
+                var deviceData = api.getDeviceState(device, HG_SID, "DeviceData");
+                getListDevices();
+                deviceData = deviceData || 'empty';
+                console.log('HG3: ', device, 'hcg:', hcg_deviceData, 'luldata:', deviceData );
+                html += '<p style="margin-left:10px; margin-top:10px">No power reporting devices for ' + device + '</p>';
+                html += '<p style="margin-left:10px; margin-top:10px">DeviceData: ' + hcg_deviceData.length + ' ' + deviceData + '</p>';
+
+                html += '<p>';
+                html += '<input type="button" value="Get Devices" onClick="HundredGraphs.packDeviceData()" style="margin-left:60%" />';
+                html += '</p>';                   
+
+            }
+
+            api.setCpanelContent(html);
+        } catch (e) {
+            Utils.logError('Error in HG.showDevices(): ' + e);
+        }
+    }  
+
     myModule = {
         uuid: uuid,
-        about: about
+        about: about,
+        getListDevices: getListDevices,
+        updateEnabled: updateEnabled,
+        showDevices: showDevices,
+        packDeviceData: packDeviceData
     };
     return myModule;
 })(api);
--- L_HundredGraphs1.lua
+++ L_HundredGraphs1.lua
@@ -13,12 +13,23 @@
 local pkg = 'L_HundredGraphs1'
 module(pkg, package.seeall)
 
-local MYSID = "urn:hundredgraphs-com:serviceId:HundredGraphs1"
-local MYTYPE = "urn:hundredgraphs-com:service:HundredGraphs:1"
+local SID = {
+	["HG"] = "urn:hundredgraphs-com:serviceId:HundredGraphs1",
+	["PM"] = "urn:micasaverde-com:serviceId:EnergyMetering1",
+	["SES"] = "urn:micasaverde-com:serviceId:SecuritySensor1"
+}
+local SRV = {
+	["PM"] = "Watts",
+	["TMP"] = "urn:micasaverde-com:serviceId:SecuritySensor1"
+}
+
+
+--local MYTYPE = "urn:hundredgraphs-com:service:HundredGraphs:1"
 local pdev
 
 -- API Key
-local API_KEY = "P8tRMZ6t" -- grab that KEY from your settings on https://www.hundredgraphs.com/settings
+-- local API_KEY = "P8tRMZ6t" -- grab that KEY from your settings on https://www.hundredgraphs.com/settings
+local API_KEY
 local NODE_ID = 1
 local TOTAL = 'Total'
 
@@ -31,6 +42,7 @@ local Log = function (text) luup.log(env .. ' Logger: ' .. (text or "empty")) en
 -- local lastFullUpload = 0
 
 local items = {} -- contains items: { time, deviceId, value }
+local g_deviceData = {}
 
 local function cDiff( dev1, dev2, svc, var )
 	-- Log(" getting calculate: " .. dev1 .. dev2 .. svc .. var)
@@ -53,12 +65,31 @@ end
 -- For function based logging, use: key, calculate, serviceVar
 -- if you want power to be counted for Total use countTotal=true
 local VARIABLES = {
-	{ key="Energy", deviceId = 301, serviceId='urn:micasaverde-com:serviceId:EnergyMetering1', serviceVar="Watts", countTotal=false }, 
-	{ key="Energy2", deviceId = 301, serviceId='urn:micasaverde-com:serviceId:EnergyMetering1', serviceVar="Watts", countTotal=true }, 
-	{ key='Lock', deviceId=437, serviceId="urn:micasaverde-com:serviceId:HaDevice1", serviceVar="BatteryLevel"},
-	{ key='Temperature', deviceId=355, serviceId="urn:upnp-org:serviceId:TemperatureSensor1", serviceVar="CurrentTemperature"},
-	{ key='Tripped', deviceId=331, serviceId="urn:micasaverde-com:serviceId:MotionSensor1", serviceVar="Tripped"},
-	{ key='Humidity', deviceId=318, serviceId="urn:micasaverde-com:serviceId:HumiditySensor1", serviceVar="CurrentLevel"},
+	{ key="House", deviceId = 301, serviceId='urn:micasaverde-com:serviceId:EnergyMetering1', serviceVar="Watts", countTotal=false }, -- Send device energy
+	{ key="HouseA", deviceId = 303, serviceId='urn:micasaverde-com:serviceId:EnergyMetering1', serviceVar="Watts", countTotal=false }, 
+	{ key="HouseB", deviceId = 304, serviceId='urn:micasaverde-com:serviceId:EnergyMetering1', serviceVar="Watts", countTotal=false }, 
+	{ key="Aquarium", deviceId = 286, serviceId='urn:micasaverde-com:serviceId:EnergyMetering1', serviceVar="Watts", countTotal=true }, 
+	{ key="pwr08", deviceId = 281, serviceId='urn:micasaverde-com:serviceId:EnergyMetering1', serviceVar="Watts", countTotal=true }, 
+	{ key="pwr04", deviceId = 285, serviceId='urn:micasaverde-com:serviceId:EnergyMetering1', serviceVar="Watts", countTotal=true },
+	{ key="pwr10_blue", deviceId = 376, serviceId='urn:micasaverde-com:serviceId:EnergyMetering1', serviceVar="Watts", countTotal=true },
+	{ key="pwr11_green", deviceId = 377, serviceId='urn:micasaverde-com:serviceId:EnergyMetering1', serviceVar="Watts", countTotal=true },
+	{ key='EntranceBtr', deviceId=331, serviceId="urn:micasaverde-com:serviceId:HaDevice1", serviceVar="BatteryLevel"},
+	{ key='GarageBtr', deviceId=320, serviceId="urn:micasaverde-com:serviceId:HaDevice1", serviceVar="BatteryLevel"},
+	{ key='OfficeBtr', deviceId=354, serviceId="urn:micasaverde-com:serviceId:HaDevice1", serviceVar="BatteryLevel"},
+	{ key='LivingBtr', deviceId=315, serviceId="urn:micasaverde-com:serviceId:HaDevice1", serviceVar="BatteryLevel"},
+	{ key='MaxBtr', deviceId=367, serviceId="urn:micasaverde-com:serviceId:HaDevice1", serviceVar="BatteryLevel"},
+	{ key='BedroomBtr', deviceId=382, serviceId="urn:micasaverde-com:serviceId:HaDevice1", serviceVar="BatteryLevel"},
+	{ key='LockBtr', deviceId=437, serviceId="urn:micasaverde-com:serviceId:HaDevice1", serviceVar="BatteryLevel"},
+	{ key='GarageTmp', deviceId=475, serviceId="urn:upnp-org:serviceId:TemperatureSensor1", serviceVar="CurrentTemperature"},
+	{ key='OfficeTmp', deviceId=355, serviceId="urn:upnp-org:serviceId:TemperatureSensor1", serviceVar="CurrentTemperature"},
+	{ key='MaxTmp', deviceId=368, serviceId="urn:upnp-org:serviceId:TemperatureSensor1", serviceVar="CurrentTemperature"},
+	{ key='BedroomTmp', deviceId=383, serviceId="urn:upnp-org:serviceId:TemperatureSensor1", serviceVar="CurrentTemperature"},
+	{ key='LivingTmp', deviceId=316, serviceId="urn:upnp-org:serviceId:TemperatureSensor1", serviceVar="CurrentTemperature"},
+	{ key='WeatherTmp', deviceId=427, serviceId="urn:upnp-org:serviceId:TemperatureSensor1", serviceVar="CurrentTemperature"},
+	{ key='EntranceSns', deviceId=331, serviceId="urn:micasaverde-com:serviceId:MotionSensor1", serviceVar="Tripped"},
+	{ key='GarageSns', deviceId=320, serviceId="urn:micasaverde-com:serviceId:SecuritySensor1", serviceVar="Tripped"},
+	{ key='OfficeSns', deviceId=354, serviceId="urn:micasaverde-com:serviceId:MotionSensor1", serviceVar="Tripped"},
+	{ key='GarageHum', deviceId=318, serviceId="urn:micasaverde-com:serviceId:HumiditySensor1", serviceVar="CurrentLevel"},
 	{ key='BedroomHum', deviceId=385, serviceId="urn:micasaverde-com:serviceId:HumiditySensor1", serviceVar="CurrentLevel"},
 	{ key='TempDiff', calculate=function() return cDiff(427, 316, "urn:upnp-org:serviceId:TemperatureSensor1", "CurrentTemperature") end, serviceVar="CurrentTemperature" } -- Send a calculated value
 --	{ key='House B', deviceId=13, serviceId="urn:upnp-org:serviceId:SwitchPower1", serviceVar="Status"}, -- Send switch status
@@ -70,6 +101,7 @@ local VARIABLES = {
 -- You shouldn't need to change anything below this line --
 local version = '6.19.2019'
 local updateInterval = 600
+local interval = 600
 local env = '[' .. pkg .. ']'
 local p = print
 local https = require "ssl.https"
@@ -77,10 +109,37 @@ local http = require('socket.http')
 https.TIMEOUT = 3
 http.TIMEOUT = 3
 
-local BASE_URL = "https://www.hundredgraphs.com/api?key=" .. API_KEY
+local BASE_URL = "https://www.hundredgraphs.com/api?key=" 
 local Log = function (text) luup.log(env .. ' Logger: ' .. (text or "empty")) end
 
-local items = {} -- contains items: { time, deviceId, value }
+local function UpdateDeviceData()
+	local deviceData = ""
+	local deviceData2 = ""
+	local items = {}
+	for devNum, info in pairs( g_deviceData ) do
+		-- dDEVICE_ID,iINACTIVITY_PERIOD,sPERIOD1_START,ePERIOD1_END,...,sPERIODn_START,ePERIODn_END,nNOTIFICATIONS_ENABLED
+		local item = "deviceId=".. devNum
+		item = item ..",key=".. info.key
+		item = item ..",serviceId=".. info.serviceId
+		item = item ..",serviceVar=".. info.serviceVar
+		-- for indexp, key in pairs(info.key) do
+		-- 	item = item ..",key=".. key
+		-- end
+		-- for indexp, serviceId in pairs(info.serviceId) do
+		-- 	item = item ..",serviceId=".. serviceId
+		-- end
+		-- for indexp, serviceVar in pairs(info.serviceVar) do
+		-- 	item = item ..",serviceVar=".. serviceVar
+		-- end
+		item = item ..",enabled=".. (info.enabled and "1" or "0")
+		table.insert( items, item )
+		deviceData = deviceData .. item .. '; '
+	end
+	deviceData2 = table.concat( items, ';' )
+	Log( " New device data: " .. deviceData .. " data2: " .. deviceData2 )
+	luup.variable_set( SID.HG, "DeviceData", deviceData, pdev )
+	luup.variable_set( SID.HG, "DeviceData2", deviceData2, pdev )
+end
 
 local function AddPair(key, value)
 	local item = string.format("%s:%s", key, value)
@@ -137,44 +196,48 @@ function HGTimerOnce()
 	return SendData()
 end
 
-function HGTimer(interval)
-	interval = interval or updateInterval
+function HGTimer()
+
+	interval = luup.variable_get( SID.HG, "Interval", pdev )
+	if (interval == nil) then
+		interval = updateInterval
+		luup.variable_set( SID.HG, "Interval", interval, pdev )
+	end
+	interval = tonumber(interval)
+	
+	API_KEY = luup.variable_get( SID.HG, "API", pdev )
+	
+	if (API_KEY == nil or API_KEY == 'empty' ) then
+		interval = 600
+		luup.call_timer("HGTimer", 1, interval, "", interval)	
+		if (DEBUG) then Log(' wrong API key: ' .. (API_KEY or "empty")) end
+		return
+	else
+		BASE_URL = "https://www.hundredgraphs.com/api?key=" .. API_KEY
+	end	
+
 	PopulateVars()
 	local code = SendData()
 	code = tonumber(code)
-	luup.variable_set( MYSID, "lastRun", code, pdev )
+	
+	-- if (code == 200) then
+	-- 	code = 'OK'
+	-- else
 	if (code == 401) then
+		luup.variable_set( SID.HG, "Enabled", 0, pdev )
 		Log(' server returned 401, your API key is wrong, HGTimer was stopped, check your lua file ')  
 		interval = 100000
 	elseif (code == 204) then
+		luup.variable_set( SID.HG, "Enabled", 0, pdev )
 		Log(' server returned 204, no data, HGTimer was stopped, check your lua file ') 
-		interval = false
+		interval = 100000
 	else
 		local res = luup.call_timer("HGTimer", 1, interval, "", interval)
 	end
+	
+	luup.variable_set( SID.HG, "lastRun", code, pdev )
 	if (DEBUG) then Log(' next in ' .. interval) end
 	return true
-end
-
--- Set or reset the current tripped state
-local function setInterval( interval, lul_device )
-	D("trip(%1,%2)", flag, lul_device)
-	local val = flag and 1 or 0
-	local currTrip = getVarNumeric( "Tripped", 0, lul_device, SECURITYSID )
-	if currTrip ~= val then
-		luup.variable_set( MYSID, "Interval", val, lul_device )
-		-- We don't need to worry about LastTrip or ArmedTripped, as Luup manages them.
-		-- Note, the semantics of ArmedTripped are such that it changes only when Armed=1
-		-- AND there's an edge (change) to Tripped. If Armed is changed from 0 to 1,
-		-- ArmedTripped is not changed, even if Tripped=1 at that moment; it will change
-		-- only when Tripped is explicitly set.
-	end
-	--[[ TripInhibit is our copy of Tripped, because Luup will change Tripped
-		 behind our back when AutoUntrip > 0--it will reset Tripped after
-		 that many seconds, but then we would turn around and set it again.
-		 We don't want to do that until Tripped resets because WE want
-		 it reset, so we use TripInhibit to lock ourselves out until then. --]]
-	-- luup.variable_set( MYSID, "TripInhibit", val, pdev )
 end
 
 _G.HGTimerOnce = HGTimerOnce
@@ -183,18 +246,56 @@ _G.HGTimer = HGTimer
 function startup(lul_device)
 	pdev = lul_device
 
-	local enabled = luup.variable_get( MYSID, "Enabled", val, pdev )	 
-	local interval = luup.variable_get( MYSID, "Interval", val, pdev )
-	if (interval == nil) then
-		interval = updateInterval
-		luup.variable_set( MYSID, "Interval", interval, pdev )
+	local deviceData = luup.variable_get( SID.HG, "DeviceData", pdev ) or ""
+	if (DEBUG) then Log(" current dev data: " .. deviceData) end
+	if deviceData == "" then
+		-- Get the list of power meters.
+		-- local g_deviceData = {}
+		local SID = {
+			["HG"] = "urn:hundredgraphs-com:serviceId:HundredGraphs1",
+			["PM"] = "urn:micasaverde-com:serviceId:EnergyMetering1",
+			["SES"] = "urn:micasaverde-com:serviceId:SecuritySensor1"
+		}
+		luup.log("HundredGraphs: SID: " .. SID.HG)
+		for devNum, devAttr in pairs( luup.devices ) do		
+			local val = luup.variable_get(SID.PM, SRV.PM, devNum)
+			if (val ~= nil) then	
+				--local desc = luup.variable_get(SID.PM, "description", devNum)			
+				luup.log("HundredGraphs: Device #" .. devNum .. " desc: " .. devAttr.description .. " KWH:" .. val)
+				for k2, v2 in pairs(devAttr) do
+					luup.log("HundredGraphs: Device #" .. devNum .. ":" .. k2 .. " = " .. tostring(v2))			
+				end
+					-- The device is a power meter.
+					--debug( " Adding device #".. devNum .."-".. devAttr.description )
+				g_deviceData[devNum] = {}
+				g_deviceData[devNum].key = devAttr.description
+				g_deviceData[devNum].serviceId = SID.PM
+				g_deviceData[devNum].serviceVar =SRV.PM
+				g_deviceData[devNum].enabled = false
+				luup.log('')
+			end
+		end
+	else
+		luup.log("HundredGraphs: existing deviceData: " .. deviceData)
 	end
 
-	Log(' Starting from plugin, ' .. MYSID .. ' dev: ' .. (pdev  or "empty") .. ' enabled: ' .. (enabled or 0) .. ' interval: ' .. interval)  
-	HGTimer(interval) return true
+	UpdateDeviceData()
+
+	local enabled = luup.variable_get( SID.HG, "Enabled", pdev )	 
+	
+	API_KEY = luup.variable_get( SID.HG, "API", pdev )
+	if (API_KEY == nil) then
+		luup.variable_set( SID.HG, "API", 'empty', pdev )
+		API_KEY = 'empty'
+	end	
+	BASE_URL = "https://www.hundredgraphs.com/api?key=" .. API_KEY
+	
+	-- Log(' Started from plugin, ' .. SID.HG .. ' dev: ' .. (pdev  or "empty") .. ' enabled: ' .. (enabled or 'disabled') .. ' API_KEY: ' .. API_KEY)  
+	HGTimer() 
+	return true
 end
 
 if (DEBUG) then Log(" *********************************************** ") end
-if (DEBUG) then Log(" started with version " .. version .. " updateInterval " .. updateInterval) end
+if (DEBUG) then Log(" Started with version " .. version) end
 
 return true
